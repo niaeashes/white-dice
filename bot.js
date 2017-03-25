d966aec7ce7d8d9876beec418cdf922425091efd
niaeashes <nia.eashes@gmail.com>
2017-03-25 18:29:45 +0900

    Support coc eval

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ require('./lib/date.extend.js')
 
 var listen = require('./lib/listen')
   , messageParser = require('./lib/messageParser.js')
+  , parameterSpace = require('./lib/parameterSpace.js')
 
 var Discord = require('discord.js');
 var client = new Discord.Client();
@@ -11,12 +12,26 @@ client.on('message', function(message) {
   console.log(message);
   data = {
     author: message.author.username,
-    msg: message.content
+    msg: message.content,
+    usedParameters: []
   };
+  var loadedParameter = parameterSpace.load(message.author.username);
+  if ( loadedParameter ) {
+    data.parameters = loadedParameter.parameters;
+  }
   messageParser(data, function(data) {
     console.log(data);
     if ( data.dice.length == 0 ) return;
-    message.channel.sendMessage("Dice: " + data.dice.join(', '));
+    var msg = [];
+    msg.push(data.dice.join(', '));
+    if ( data.usedParameters.length > 0 ) {
+      msg.push('')
+    }
+    for ( var i in data.usedParameters ) {
+      var parameter = data.usedParameters[i]
+      msg.push(parameter.name+": "+parameter.value);
+    }
+    message.channel.sendMessage(msg.join("\n"))
   });
 });
 
