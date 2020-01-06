3020be52174eed53633acb50aff8af5bafe5d0d1
Nia <nia.eashes@gmail.com>
2020-01-06 10:40:59 +0900

    Update character roll

--- bot.js
+++ bot.js
@@ -9,7 +9,6 @@ var Discord = require('discord.js');
 var client = new Discord.Client();
 
 client.on('message', function(message) {
-  console.log(message);
   data = {
     author: message.author.username,
     msg: message.content,
@@ -21,10 +20,15 @@ client.on('message', function(message) {
     data.parameters = loadedParameter.parameters;
   }
   messageParser(data, function(data) {
-    console.log(data);
     if ( data.dice.length == 0 ) return;
     var msg = [];
-    msg.push(data.dice.join(', '));
+    if (data.multiline) {
+      for (var i in data.dice) {
+        msg.push(data.dice[i])
+      }
+    } else {
+      msg.push(data.dice.join(', '));
+    }
     if ( data.usedParameters.length > 0 ) {
       msg.push('')
     }
@@ -32,7 +36,7 @@ client.on('message', function(message) {
       var parameter = data.usedParameters[i]
       msg.push(parameter.name+": "+parameter.value);
     }
-    message.channel.sendMessage(msg.join("\n"))
+    message.channel.send(msg.join("\n"))
   });
 });
 
